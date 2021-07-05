861a9b7797a98f3ed041a11be7027ed0ba66feb8
jdwns96 <djjng3@gmail.com>
2021-07-05 10:29:28 +0900

    git rf

---
+++ view/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-4 h-4">
+        asd
+      </div>
+    </div>
+  );
+}
+
+export default App;
